ae9939cc9c1e8a016a1c5f04eb7baf88a2790ad3
gonkaos <arthur.cavalcante.puc@gmail.com>
2018-05-05 01:52:36 -0300

    Função de criação da lista de cartas funcionando corretamente, falta criar as cartas e passa-las como ponteiro para a lista

--- T2/T2-Linux/teste.c
+++ T2/T2-Linux/teste.c
@@ -5,10 +5,13 @@
 
 int main()
 {
+	int retorno;
+	BAR_tppBaralho *Baralho=(BAR_tppBaralho*)malloc(sizeof(BAR_tppBaralho));
 	BAR_tppCarta *carta=(BAR_tppCarta*)malloc(sizeof(BAR_tppCarta));
-  if(BAR_CriaCarta(carta,"A",10,"ouros")==BAR_CondRetOK)
+	if(BAR_CriaCarta(carta,"A",10,"ouros")==BAR_CondRetOK)
 	{
-	printf("sucesso!\n");
+	printf("sucesso em criar uma carta!\n");
 	}
+	retorno=BAR_CriarBaralho(Baralho);
 	return 0;
 }
